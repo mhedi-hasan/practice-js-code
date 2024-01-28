f7bb3e4d06483b1aa7d4157e5aea89aef9b23122
mehedi-hasan <mhedih32@gmail.com>
2024-01-28 21:32:24 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,63 @@
+// Number type data
+var price = 22;
+console.log(price)
+
+var rate = 600;
+console.log(rate)
+
+console.log(typeof price)
+console.log(typeof rate)
+
+// String type data
+var country = ('Bangladesh')
+console.log(country)
+
+var subject = "English";
+console.log(subject)
+
+console.log(typeof country)
+console.log(subject)
+
+// Boolean type data
+var fail = true;
+console.log(fail)
+
+var dakat = false;
+console.log(dakat)
+
+console.log(typeof fail)
+console.log(dakat)
+
+var roll = 0.40;
+console.log(roll)
+console.log(typeof roll)
+
+var mother = 'Mehedi Hasan';
+console.log(mother)
+console.log(typeof mother)
+
+var subject_code = 610;
+console.log(subject_code)
+console.log(typeof subject_code)
+
+var maths_code = parseInt("246546");
+var math_code = parseFloat("10.511");
+console.log(math_code)
+console.log(typeof math_code)
+
+console.log(maths_code+math_code);
+console.log(subject_code+math_code);
+
+var first = 0.1;
+var second = 0.2;
+var total = first+second;
+console.log(total.toFixed(1))
+
+var five = 0.8;
+var six = pars('0.9');
+var total = five+six;
+console.log(total.toFixed(4))
+
+const num = -40 / 0;
+console.log(num)
+
---
+++ opertation.js
@@ -0,0 +1,14 @@
+// *********** remainder *************
+// console.log(10/9);
+// console.log(50/37);
+// console.log(10%3);
+// console.log(50%47);
+// ------------------ --------------------
+var price = 50;
+price = price+40;
+console.log(price)
+
+var newPrice = price + 12;
+
+var discountPrice = newPrice - 20;
+console.log(discountPrice)

0d94f69b207b5e0aed326a3f2122a1c499098c73
mehedi-hasan <mhedih32@gmail.com>
2024-01-28 22:52:47 +0600

    updated task file

--- task.js
+++ task.js
@@ -13,13 +13,44 @@
 // console.log(roll)
 // // -------------------------------------------------
 // // ------------  2. Add or remove elements ----------- //
-let plase = ['Seant Martain',"Cox's Bazar",'Guliya Khali']
-plase.push('RangaMati')
-plase.unshift('Bashbadiya','Bandarban')
-// plase.shift()
-plase.push('RangaMati')
-plase.pop()
-console.log(plase)
+// let plase = ['Seant Martain',"Cox's Bazar",'Guliya Khali']
+// plase.push('RangaMati')
+// plase.unshift('Bashbadiya','Bandarban')
+// // plase.shift()
+// plase.push('RangaMati')
+// plase.pop()
+// console.log(plase)
 // --------------------------------------------------
 // -------- 3. Checking Array Membership with ‘includes’ ---------- //
 // ---------**Instructions:**--------------
+// let book =['Math','English','Bangla','Physics','Python Programming']
+// // console.log(books.includes('Math'))
+// if(book.includes('Math')){
+//     console.log('This Book Is Here.Please Take Now')
+// }
+// else{
+//     // console.log('Your Book Is Not Here.Please Cheek In Another Sides')
+// }
+// ------------- false value----------------
+// let books =['Math','English','Bangla','Physics','Python Programming']
+// console.log(books.includes('Math'))
+// if(books.includes('Social Science')){
+//     console.log('This Books Is Here.Please Take Now')
+// }
+// else{
+//     console.log('Your Books Is Not Here.Please Cheek In Another Sides')
+// }
+// ------------------------------------------
+// --------------- 4. Checking if it's an Array ------------------
+// -------------- **Instructions:** ---------------
+// 1. Create different variables, each containing either an array or a non-array value.
+// 2. Now use isArray to check if each variable is an array.
+// 3. Print a message to the console indicating whether each variable is an array or not.
+// ------------------------------------------
+// ----------------- 5. Combining Arrays --------------
+
+// -------------- **Instructions:** -----------------------
+// 1. Create two arrays of your choice.
+// 2. Use the concat method to combine the two arrays into a new array.
+// 3. Print both the original arrays and the combined array using console.log().
+// --------------------------------

a29230e1364894c77433ca4a571bcaef32c35d5d
mehedi-hasan <mhedih32@gmail.com>
2024-01-28 22:33:19 +0600

    uploded 2 file

---
+++ indexOf.js
@@ -0,0 +1,9 @@
+let number = [10,50,51,11,41,81,11,91];
+console.log(number.indexOf(91))
+if(number.includes(50)){
+    console.log('How Can I Help You Sir')
+}
+else{
+    console.log('Please Sir You Are Not Comming To The In Sides')
+}
+console.log(number.includes(40))
---
+++ shift-unshift.js
@@ -0,0 +1,26 @@
+let number = [10,50,51,11,41,81,11,91];
+console.log(number)
+number.shift();
+number.shift();
+console.log(number)
+number.unshift(1000)
+number.unshift(7000)
+console.log(number)
+number.shift();
+console.log(number)
+number.pop()
+console.log(number)
+number.push(9000)
+console.log(number)
+number.pop();
+console.log(number)
+number.shift();
+console.log(number)
+number.unshift(66666)
+console.log(number)
+number.shift();
+console.log(number)
+number.push('Mehedi Hasan')
+console.log(number)
+number.pop();
+console.log(number)

87e9156f47c2fee4c78110b73b622028480c1fe9
mehedi-hasan <mhedih32@gmail.com>
2024-01-29 15:59:48 +0600

    new 2 file added

---
+++ bonus.js
@@ -0,0 +1,34 @@
+// switch use kore 7 days er name print korlam 
+const dayOfWeek = 4;
+switch (dayOfWeek){
+    case 1:
+        console.log('Saturday')
+        break;
+    case 2:
+        console.log('Sunday')
+        break;
+    case 3:
+        console.log('Monday')
+        break;
+    case 4:
+        console.log('Tuesday')
+        break;
+    case 5:
+        console.log('Wednesday')
+        break;
+    case 6:
+        console.log('Thursday')
+        break;
+    case 7:
+        console.log('Friday')
+        break;
+    default:
+        console.log('Invalid day')
+}
+// --------------------------------------------------
+// const roll = [10,22,47,62,65]
+// console.log(roll)
+// console.log(typeof roll)
+// console.log(roll.indexOf(65))
+// console.log(roll.includes(10))
+// console.log(roll.includes(50))

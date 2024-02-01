885e868ade6481af41a50cebfec875d90f56933f
Mehedi Hasan <mhedih32@gmail.com>
2024-02-01 16:20:00 +0600

    create and uploded four file

---
+++ for.js
@@ -0,0 +1,49 @@
+// // for loop er kaj dekhano holo
+// const frutsName = ["mango", "orange", "banana", "apple", "watermelon"];
+// console.log(frutsName)
+// // ---------- 1st ----------
+// for (const frutss of frutsName) {
+// console.log(frutss);
+// console.log('I want to eat')
+// console.log('I love all fruts very much')
+// }
+// // ---------- 2nd ------------
+// for(const foll of frutsName){
+//     console.log(foll)
+//     console.log('All fruts is very testy')
+// }
+// // -------------- 3rd -------------
+// const roll = [10, 20, 30, 40, 50, 60, 70, 80, 90, 12, 23, 50, 55, 44, 78];
+// console.log(roll);
+// for (const rolos of roll) {
+// console.log(rolos);
+// console.log('This is a number')
+// }
+// // -------------- 2nd 3rd -------------
+// console.log(roll.length)
+// for (const number of roll){
+//     console.log(number)
+//     console.log('This is math number')
+// }
+// // -------------- 4th -------------
+// const fullName = ["Mehedi Hasna", "Rokib Hossin", "Abdul kader", "Shaffuddin","Hashim Ali"];
+// console.log(fullName)
+
+// for(const names of fullName){
+//     console.log(names)
+//     console.log( typeof names)
+//     console.log('Legendary name of the Bangladesh')
+// }
+// console.log(fullName.length)
+// for (const num of fullName){ 
+//     console.log(num)
+//     console.log(typeof num)
+//     console.log('Very nice name')
+// }
+// for(const nams of fullName){
+//     console.log(nams)
+//     console.log(typeof nams)
+//     console.log('Wonderful Name in the country')
+    
+// }
+// // --------------------------
---
+++ for2.js
@@ -0,0 +1,30 @@
+let num = 2; // loop variable
+while (num < 5) {
+  // loop condition
+  console.log(num); // loop statement
+  num++; // loop variable changes
+}
+// for ( loop variable; loop declaration; loop condition;)
+for (num = 0; num < 5; num++) {
+  console.log("mehedi", num);
+}
+
+// --------------task----------
+// ------------while diya try korchi--------
+let nums = 1;
+while (nums < 50) {
+    console.log("I love my Family", nums);
+    nums++;
+}
+// ------------for diya try korchi--------
+for (nuem = 1; nuem <= 50; nuem++) {
+    console.log('Mehedi Hasan',nuem);
+}
+// even number
+for (nuem = 1; nuem <= 50; nuem=nuem+2) {
+    console.log(nuem);
+}
+// odd number
+for (nuem = 0; nuem <= 50; nuem=nuem+2) {
+    console.log(nuem);
+}
